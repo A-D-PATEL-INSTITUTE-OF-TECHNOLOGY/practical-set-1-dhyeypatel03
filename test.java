3900a8eff8f98446232267d5d9ad2a9f541c3961
daptop03 <dap7203@gmail.com>
2023-01-04 15:30:22 +0530

    FIRSTCOMMIT

---
+++ test.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main(Character[] a) {
+            System.out.println("Hello world");
+            System.out.println("Hello dhyey");
+    }
+}
+
+class Test2 {
+    public static void main(Character[] a) {
+            System.out.println("Hello world");
+            System.out.println("Hello dhyey");
+    }
+}
